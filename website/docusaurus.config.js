f3b5241dfcb247350afe75ef8416971db472a215
yangfengzzz <yangfengzzz@hotmail.com>
2022-02-07 13:20:46 +0800

    update sites

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://digitalarche.netlify.app',
-  baseUrl: '/',
+  baseUrl: '/website',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
